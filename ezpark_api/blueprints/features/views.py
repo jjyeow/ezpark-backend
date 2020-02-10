4bd377e6730916b1103246f7fbeeba5110f3adce
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-02-10 10:40:44 +0800

    added history api feature, test run 1

--- ezpark_api/blueprints/features/views.py
+++ ezpark_api/blueprints/features/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, json, jsonify
 from models.mall import Mall
+from models.history import History
+from models.parking import Parking
+from models.floor import Floor
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash
@@ -23,6 +26,37 @@ def index():
 
     return jsonify(responseObj), 200
 
+@features_api_blueprint.route('/history', methods=['GET'])
+@jwt_required
+def history(): 
+    user_id = get_jwt_identity()
+    current_user = User.get_by_id(user_id)
+    history_obj = History.get_or_none(user_id = user_id)
+    history_arr = []
+
+    for history in history_obj: 
+        history_list = {
+            'floor': Floor.get_by_id(Parking.get_by_id(history.parking_id).floor_id).floor,
+            'parking': Parking.get_by_id(history.parking_id).parking_num
+            'start': history.created_at
+        }
+        history_arr.append(history_list)
+
+    if history_obj: 
+        responseObj = {
+            'status': 'success',
+            'history': history_arr
+        }
+
+        return jsonify(responseObj), 200
+
+    else: 
+        responseObj = {
+            'status': 'success',
+            'history': 'No history'
+        }
+        
+        return jsonify(responseObj), 200
 
 
 
