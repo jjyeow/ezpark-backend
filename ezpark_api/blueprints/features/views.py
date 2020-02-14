5f5ff9c28cef4b1b1d7b196ae8b6c4958e1a838c
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-02-14 18:53:26 +0800

    fixed another dumb errors

--- ezpark_api/blueprints/features/views.py
+++ ezpark_api/blueprints/features/views.py
@@ -197,7 +197,7 @@ def layout_id():
     user_id = get_jwt_identity()
     mall_id = request.json.get('mall_id')
     mall_inst = Mall.get_by_id(mall_id)
-    floors = Floor.select().where(Mall.mall_id == mall_id).order_by(Mall.id.asc())
+    floors = Floor.select().where(Floor.mall_id == mall_inst.id).order_by(Mall.id.asc())
     floor_arr = []
     parking_arr = []
     parking_arr1 = []
