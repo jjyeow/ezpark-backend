a0dfd6f52a49fe8ce86b544e55229efaaf257f16
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-02-16 17:37:22 +0800

    separated amount of car park from the floor name array

--- ezpark_api/blueprints/features/views.py
+++ ezpark_api/blueprints/features/views.py
@@ -220,11 +220,13 @@ def layout_id():
     mall_inst = Mall.get_by_id(mall_id)
     floors = Floor.select().where(Floor.mall_id == mall_inst.id).order_by(Floor.id.asc())
     floor_arr = []
+    amount_arr = []
     parking_arr = []
     parking_arr1 = []
     parking_arr2 = []
     for floor in floors: 
-        floor_arr.append(floor.floor+' ('+str(floor.count_available())+')')
+        floor_arr.append(floor.floor)
+        amount_arr.append(' ('+str(floor.count_available())+')')
         # for parking in floor.parking:
         #     parking_arr.append({"id": parking.id, "status": parking.status})
     floor1 = Floor.get_or_none(floor = floor_arr[0])
@@ -246,7 +248,8 @@ def layout_id():
             'id': mall_inst.id,
             'floor': floor_arr,
             'parking1': parking_arr1,
-            'parking2': parking_arr2
+            'parking2': parking_arr2,
+            'amount': amount_arr 
         }
 
         return jsonify(responseObj), 200
